65f3005666e0e084b9b5559d510a8e56cf768eb2
Kaoru Matsumoto <kmatsu@gmail.com>
2013-07-10 16:49:59 +0000

    updated

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new Buffer ();
+var buffer = new Buffer (256);
   response.send(buffer.toString('utf8', fs.readFileSync('index.html')));
  });
 
